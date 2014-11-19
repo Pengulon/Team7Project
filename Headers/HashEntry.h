fd9a3e652697174c1eb4229a61d6a7c709e36ee7
Brandon Chai <brandonlchai@gmail.com>
2014-11-19 00:17:21 -0800

    added Hash entry header

---
+++ Headers/HashEntry.h
@@ -0,0 +1,51 @@
+// Brandon Chai, 2014
+// HashEntry for a Hash Map
+
+#ifndef HASHENTRY_H_INCLUDED
+#define HASHENTRY_H_INCLUDED
+#include <iostream>
+
+template<class KeyType, class ItemType>
+class HashEntry
+{
+private:
+    ItemType item;
+    KeyType key;
+    HashEntry<KeyType, ItemType>* nextPtr;
+public:
+    // Constructors
+    HashEntry() {item = NULL; key = ""; nextPtr = NULL;}
+    HashEntry(ItemType newEntry, KeyType newKey,
+              HashEntry<KeyType, ItemType>* nextEntryPtr) {item = newEntry; key = newKey; nextPtr = nextEntryPtr;}
+
+    // Accessors
+    ItemType getItem() const {return item;}
+    KeyType getKey() const {return key;}
+    HashEntry<KeyType, ItemType>* getNext() const {return nextPtr;}
+
+    // Mutators
+    void setItem(const ItemType& newEntry) {item = newEntry;}
+    void setKey(const KeyType& newKey) {key = newKey;}
+    void setNext(HashEntry<KeyType, ItemType>* nextEntryPtr) {nextPtr = nextEntryPtr;}
+
+    // Overloaded operators
+    bool operator==(const HashEntry<KeyType, ItemType>& rightHandItem) const;
+    bool operator>(const HashEntry<KeyType, ItemType>& rightHandItem) const;
+};
+
+// == - Checks to see if value is same key
+template <class KeyType, class ItemType>
+bool HashEntry<KeyType, ItemType>::operator==
+                    (const HashEntry<KeyType, ItemType>& rightHandItem) const
+{
+    return (key == rightHandItem.getKey());
+}
+
+// == - Checks to see if value is greater than key
+template <class KeyType, class ItemType>
+bool HashEntry<KeyType, ItemType>::operator>
+                    (const HashEntry<KeyType, ItemType>& rightHandItem) const
+{
+    return (key > rightHandItem.getKey());
+}
+#endif // HASHENTRY_H_INCLUDED
